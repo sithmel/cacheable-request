c79666789945c79a10583510aa9caec4c27c7ae1
Luke Childs <lukechilds123@gmail.com>
2017-06-08 11:49:08 +0700

    test cacheableRequest(request) returns a function

--- test/cacheable-request.js
+++ test/cacheable-request.js
@@ -1,6 +1,11 @@
+import { request } from 'http';
 import test from 'ava';
 import cacheableRequest from '../';
 
 test('cacheableRequest is a function', t => {
 	t.is(typeof cacheableRequest, 'function');
+});
+
+test('cacheableRequest(request) returns a function', t => {
+	t.is(typeof cacheableRequest(request), 'function');
 });
