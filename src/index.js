ac38fc5d2da56e3ab973f7ac0b229d48a213057c
Luke Childs <lukechilds123@gmail.com>
2017-06-08 11:09:11 +0700

    Wrap native request

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 'use strict';
 
-const cacheableRequest = () => {
+const EventEmitter = require('events');
 
+const cacheableRequest = request => (opts, cb) => {
+	opts = opts || {};
+
+	const ee = new EventEmitter();
+
+	const get = opts => {
+		const req = request(opts, cb);
+		ee.emit('request', req);
+	};
+
+	setImmediate(() => get(opts));
+
+	return ee;
 };
 
 module.exports = cacheableRequest;
