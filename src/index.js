a26df787235619ca4dc3c307834b946c32e00db4
Luke Childs <lukechilds123@gmail.com>
2017-06-08 15:10:41 +0700

    Store cacheable responses

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
 'use strict';
 
 const EventEmitter = require('events');
+const urlLib = require('url');
+const normalizeUrl = require('normalize-url');
+const getStream = require('get-stream');
+const CachePolicy = require('http-cache-semantics');
 
-const cacheableRequest = request => (opts, cb) => {
-	opts = opts || {};
+const cacheKey = opts => {
+	const url = normalizeUrl(urlLib.format(opts));
+	return `${opts.method}:${url}`;
+};
+
+const cacheableRequest = (request, cache) => (opts, cb) => {
+	opts = Object.assign({
+    headers: {},
+    method: 'GET'
+  }, opts);
 
 	const ee = new EventEmitter();
 
 	const get = opts => {
-		const req = request(opts, cb);
+		const req = request(opts, response => {
+			response.cachePolicy = new CachePolicy(opts, response);
+
+			if (cache && response.cachePolicy.storable()) {
+				getStream.buffer(response).then(body => {
+					const key = cacheKey(opts);
+					const value = {
+						cachePolicy: response.cachePolicy.toObject(),
+						url: response.url,
+						statusCode: response.statusCode,
+						body
+					};
+					const ttl = response.cachePolicy.timeToLive();
+					cache.set(key, value, ttl);
+				});
+			}
+
+			if (typeof cb === 'function') {
+				cb(response);
+			}
+		});
 		ee.emit('request', req);
 	};
 
