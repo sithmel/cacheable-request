45d34014ae37fd531f7ec644ee8d7df102132177
Luke Childs <lukechilds123@gmail.com>
2017-06-12 15:08:05 +0700

    Test cacheableRequest returns an event emitter

--- test/cacheable-request.js
+++ test/cacheable-request.js
@@ -1,6 +1,25 @@
+import EventEmitter from 'events';
+import { request } from 'http';
 import test from 'ava';
+import createTestServer from 'create-test-server';
 import cacheableRequest from '../';
+
+let s;
+
+test.before('setup', async () => {
+	s = await createTestServer();
+	s.get('/', (req, res) => res.end('hi'));
+});
 
 test('cacheableRequest is a function', t => {
 	t.is(typeof cacheableRequest, 'function');
+});
+
+test.cb('cacheableRequest returns an event emitter', t => {
+	const returnValue = cacheableRequest(request, s.url, () => t.end()).on('request', req => req.end());
+	t.true(returnValue instanceof EventEmitter);
+});
+
+test.after('cleanup', async () => {
+	await s.close();
 });
