a8be098682e5279408aa9fbffac90390080a1fbb
Aditya Patadia <aditya@patadia.org>
2019-01-14 14:30:05 +0530

    makerequest is now saperate function.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const lowercaseKeys = require('lowercase-keys');
 const cloneResponse = require('clone-response');
 const Keyv = require('keyv');
 
-function prepareOptsAndURL(opts) {
+const prepareOptsAndURL = opts => {
 	let url;
 	if (typeof opts === 'string') {
 		url = normalizeUrlObject(urlLib.parse(opts));
@@ -67,6 +67,95 @@ const getCachedResponse = async (opts, cache, key) => {
 	return { cacheEntry, policy };
 };
 
+const makeRequest = (request, revalidate, opts, madeRequest, cache, key, ee, cb) => {
+	madeRequest = true;
+	let requestErrored = false;
+	let requestErrorCallback;
+
+	const requestErrorPromise = new Promise(resolve => {
+		requestErrorCallback = () => {
+			if (!requestErrored) {
+				requestErrored = true;
+				resolve();
+			}
+		};
+	});
+
+	const handler = async response => {
+		if (revalidate && !opts.forceRefresh) {
+			response.status = response.statusCode;
+			const revalidatedPolicy = CachePolicy.fromObject(revalidate.cachePolicy).revalidatedPolicy(opts, response);
+			if (!revalidatedPolicy.modified) {
+				const headers = revalidatedPolicy.policy.responseHeaders();
+				response = new Response(revalidate.statusCode, headers, revalidate.body, revalidate.url);
+				response.cachePolicy = revalidatedPolicy.policy;
+				response.fromCache = true;
+			}
+		}
+
+		if (!response.fromCache) {
+			response.cachePolicy = new CachePolicy(opts, response, opts);
+			response.fromCache = false;
+		}
+
+		let clonedResponse;
+		if (opts.cache && response.cachePolicy.storable()) {
+			clonedResponse = cloneResponse(response);
+
+			try {
+				const bodyPromise = getStream.buffer(response);
+
+				await Promise.race([
+					requestErrorPromise,
+					new Promise(resolve => response.once('end', resolve))
+				]);
+
+				if (requestErrored) {
+					return;
+				}
+
+				const body = await bodyPromise;
+
+				const value = {
+					cachePolicy: response.cachePolicy.toObject(),
+					url: response.url,
+					statusCode: response.fromCache ? revalidate.statusCode : response.statusCode,
+					body
+				};
+
+				let ttl = opts.strictTtl ? response.cachePolicy.timeToLive() : undefined;
+				if (opts.maxTtl) {
+					ttl = ttl ? Math.min(ttl, opts.maxTtl) : opts.maxTtl;
+				}
+				await cache.set(key, value, ttl);
+			} catch (error) {
+				ee.emit('error', new CacheableRequest.CacheError(error));
+			}
+		} else if (opts.cache && revalidate) {
+			try {
+				await cache.delete(key);
+			} catch (error) {
+				ee.emit('error', new CacheableRequest.CacheError(error));
+			}
+		}
+
+		ee.emit('response', clonedResponse || response);
+		if (typeof cb === 'function') {
+			cb(clonedResponse || response);
+		}
+	};
+
+
+	try {
+		const req = request(opts, handler);
+		req.once('error', requestErrorCallback);
+		req.once('abort', requestErrorCallback);
+		ee.emit('request', req);
+	} catch (error) {
+		ee.emit('error', new CacheableRequest.RequestError(error));
+	}
+};
+
 class CacheableRequest {
 	constructor(request, cacheAdapter) {
 		if (typeof request !== 'function') {
@@ -93,94 +182,6 @@ class CacheableRequest {
 			let revalidate = false;
 			let madeRequest = false;
 
-			const makeRequest = opts => {
-				madeRequest = true;
-				let requestErrored = false;
-				let requestErrorCallback;
-
-				const requestErrorPromise = new Promise(resolve => {
-					requestErrorCallback = () => {
-						if (!requestErrored) {
-							requestErrored = true;
-							resolve();
-						}
-					};
-				});
-
-				const handler = async response => {
-					if (revalidate && !opts.forceRefresh) {
-						response.status = response.statusCode;
-						const revalidatedPolicy = CachePolicy.fromObject(revalidate.cachePolicy).revalidatedPolicy(opts, response);
-						if (!revalidatedPolicy.modified) {
-							const headers = revalidatedPolicy.policy.responseHeaders();
-							response = new Response(revalidate.statusCode, headers, revalidate.body, revalidate.url);
-							response.cachePolicy = revalidatedPolicy.policy;
-							response.fromCache = true;
-						}
-					}
-
-					if (!response.fromCache) {
-						response.cachePolicy = new CachePolicy(opts, response, opts);
-						response.fromCache = false;
-					}
-
-					let clonedResponse;
-					if (opts.cache && response.cachePolicy.storable()) {
-						clonedResponse = cloneResponse(response);
-
-						try {
-							const bodyPromise = getStream.buffer(response);
-
-							await Promise.race([
-								requestErrorPromise,
-								new Promise(resolve => response.once('end', resolve))
-							]);
-
-							if (requestErrored) {
-								return;
-							}
-
-							const body = await bodyPromise;
-
-							const value = {
-								cachePolicy: response.cachePolicy.toObject(),
-								url: response.url,
-								statusCode: response.fromCache ? revalidate.statusCode : response.statusCode,
-								body
-							};
-
-							let ttl = opts.strictTtl ? response.cachePolicy.timeToLive() : undefined;
-							if (opts.maxTtl) {
-								ttl = ttl ? Math.min(ttl, opts.maxTtl) : opts.maxTtl;
-							}
-							await this.cache.set(key, value, ttl);
-						} catch (error) {
-							ee.emit('error', new CacheableRequest.CacheError(error));
-						}
-					} else if (opts.cache && revalidate) {
-						try {
-							await this.cache.delete(key);
-						} catch (error) {
-							ee.emit('error', new CacheableRequest.CacheError(error));
-						}
-					}
-
-					ee.emit('response', clonedResponse || response);
-					if (typeof cb === 'function') {
-						cb(clonedResponse || response);
-					}
-				};
-
-				try {
-					const req = request(opts, handler);
-					req.once('error', requestErrorCallback);
-					req.once('abort', requestErrorCallback);
-					ee.emit('request', req);
-				} catch (error) {
-					ee.emit('error', new CacheableRequest.RequestError(error));
-				}
-			};
-
 			// This is a small memory leak. Need to correct it.
 			this.cache.on('error', err => ee.emit('error', new CacheableRequest.CacheError(err)));
 
@@ -188,7 +189,7 @@ class CacheableRequest {
 				try {
 					const data = await getCachedResponse(opts, this.cache, key);
 					if (data.response) {
-					// Response is found in cache and it's valid response.
+					// Response is found in cache and it's VALID response.
 					// no need to make request. return the response from cache.
 
 						ee.emit('response', data.response);
@@ -196,22 +197,22 @@ class CacheableRequest {
 							cb(data.response);
 						}
 					} else if (data.cacheEntry) {
-					// Response is found in cache but we need to revalidate it.
+					// Response is found in cache but we need to REVALIDATE it.
 					// setting appropriate headers and making request to revalidate.
 
 						revalidate = data.cacheEntry;
 						opts.headers = data.policy.revalidationHeaders(opts);
-						makeRequest(opts);
+						makeRequest(request, revalidate, opts, madeRequest, this.cache, key, ee, cb);
 					} else {
-					// Response is not found in cache. Making normal request.
-						makeRequest(opts);
+					// Response is NOT FOUND in cache. Making normal request.
+						makeRequest(request, revalidate, opts, madeRequest, this.cache, key, ee, cb);
 					}
 				} catch (error) {
 					if (opts.automaticFailover && !madeRequest) {
 					// Request is not fired and automaticfailver is set.
 					// this typically happens when cache can't be obtained from store.
 					// we will make normal request without waiting for cache.
-						makeRequest(opts);
+						makeRequest(request, revalidate, opts, madeRequest, this.cache, key, ee, cb);
 					}
 					// This still emits error about cache.
 					ee.emit('error', new CacheableRequest.CacheError(error));
